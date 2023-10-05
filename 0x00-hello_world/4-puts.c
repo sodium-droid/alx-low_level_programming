06171ecd9d3e66f12919a069b75505563c025d79
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-05 15:34:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - A function that prints a string with puts funtion.
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
