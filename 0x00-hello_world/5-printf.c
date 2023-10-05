c734b506912fba1d144cb449b1f29bb4039802e6
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-05 15:42:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - A function that prints a string with printf funtion.
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
