59d09d01548e7be15e5903f7fa4cc2708860fd28
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-06 00:28:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - A c program that
+ *prints to standard error (stderr)
+ *Return 1 (Error)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
