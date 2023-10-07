0bd2b8f38b2b6b07d67405e398a6fcee458120a3
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-07 06:15:20 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - A function to print
+*the type of a random integer
+* Return: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+if ((n % 10) > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+}
+else if (n == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, n);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+}
+return (0);
+}
