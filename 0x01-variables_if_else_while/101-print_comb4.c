b2ba7cf80513e34280a11f0edf373c046ee85202
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-08 14:02:36 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - A script that prints combination
+*of two digits without repitition and
+*with commas and spaces.
+*Return: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+	int o, n, m;
+
+	for (n = 0; n <= 7; n++)
+{
+		for (m = n + 1; m <= 8; m++)
+{
+			for (o = m + 1; o <= 9; o++)
+{
+				putchar(n + '0');
+				putchar(m + '0');
+				putchar(o + '0');
+	if (n != 8 || m != 9)
+{
+		putchar(44);
+		putchar(32);
+}
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
