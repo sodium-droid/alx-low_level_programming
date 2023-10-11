d11bd5e8fc912d5cf371b16280605d1c30dd313d
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-11 13:38:30 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_sign - function that prints
+* a sign based on number's
+* relationship with '0'.
+* @n: number to check
+* Return: 0, 1 & -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
