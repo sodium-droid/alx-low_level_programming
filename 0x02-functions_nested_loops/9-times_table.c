919ce06d87ff86e4815754f9d7f9cd2822ee44f3
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-12 06:23:20 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar((i * j) + '0');
+_putchar((i * j));
 if (j < 9)
 {
 _putchar(',');
@@ -22,5 +22,4 @@ _putchar(' ');
 }
 _putchar('\n');
 }
-return (0);
 }
