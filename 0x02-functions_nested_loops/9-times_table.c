03c8f89a20427faec3dad4ee9fd760cd128a4a7f
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-12 05:43:12 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - A function that
+ * prints numbers in multiples of 0-9.
+ * Return: 0
+ */
+
+void times_table(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+_putchar((i * j) + '0');
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
