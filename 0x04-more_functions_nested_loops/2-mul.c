1bf69a0a90bcb26b6252733ed27e115137b69bb3
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-13 08:07:29 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul -a function that prints
+ *the product of two numbers
+ *@a: A number
+ *@b: another number
+ *Return: 0 (a * b)
+ */
+
+int mul(int a, int b)
+{
+int result = a * b
+
+	return (result);
+}
