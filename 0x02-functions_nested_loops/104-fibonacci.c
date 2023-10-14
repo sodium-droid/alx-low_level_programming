fe24d7881d37a579de78951ecfe201787454324a
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-14 16:09:34 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib2_half1, fib2_half2;
+	unsigned long fib1_half2, fib1_half1, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
@@ -21,6 +21,7 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
+
 	for (count = 93; count < 99; count++)
 {
 	half1 = fib1_half1 + fib2_half1;
@@ -41,4 +42,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
