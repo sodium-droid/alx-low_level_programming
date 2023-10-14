a478d550594d6ae29751da759c80e35be49fe696
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-14 10:04:24 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**add - a funtion that adds
+ * two integers
+ *@i: 1 integer
+ *@j: 2 integer
+ *Return: sum of integers
+ */
+
+int add(int i, int j)
+{
+
+int result = i + j;
+
+return (result);
+}
