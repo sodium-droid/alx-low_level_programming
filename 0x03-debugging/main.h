a6a23e57ccb2a586f1b92b8a554a95e3ead3e4e8
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-14 08:43:09 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
