46d0566045326dc85d1714b7ef2e6f1a1f109207
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-16 12:00:57 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,12 @@
 
 void puts2(char *str)
 {
-int i = 0;
+int i;
 
-while (str[i] != '\0')
+for (i = 0; str[i] != '\0'; i++)
+if (i % 2 == 0)
 {
 _putchar(str[i]);
-i = i + 2;
 }
 _putchar('\n');
 }
