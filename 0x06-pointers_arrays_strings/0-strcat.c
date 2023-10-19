2acec62c1d5acb9af1b09268828448de82f20570
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-19 11:21:21 -0700

    0x06-pointers_arrays_strings/

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strcat - A funtion that
+ *concats 2 strings
+ *@dest: string 1
+ *@src: string 2
+ *Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i, lena = _strlen(src), lenb = _strlen(dest);
+
+for (i = 0; i <= lena; i++)
+{
+	dest[lenb + i] = src[i];
+}
+return (dest);
+}
+/**
+ *_strlen - function that
+ *outputs the lenght of a string
+ *@s: required string
+ *Return: 0 (lenght of string)
+ */
+int _strlen(char *s)
+{
+int i;
+int j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	j++;
+return (j);
+}
