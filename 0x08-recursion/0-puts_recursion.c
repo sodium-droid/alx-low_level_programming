16e7da106e7f5d81bbf49d60197b3ea90ee09563
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-25 13:37:49 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts_recursion - function that
+ *prints a string by recursion.
+ *@s: pointer to string
+ * Return: *s
+ */
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+	_putchar(*s);
+	_puts_recursion(s++);
+}
+else
+_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory is for recursion - function that calls itself to solve small repetitive tasks to solve a given task
