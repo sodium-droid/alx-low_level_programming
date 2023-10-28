07efbae25f305ee65d0c8e01a3f19a3a38efac00
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-10-28 05:57:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - a function that prints
+ *its name.
+ *@argc: count
+ *@argv: vector
+ *Return: name (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory  is for arguments supplied to functions, especially main function
