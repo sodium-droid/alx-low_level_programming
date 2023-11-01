4a5e24564afd67240c96bcb933c623bc53ee5703
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-11-01 08:15:20 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,22 +16,29 @@ int i, j;
 if (width <= 0 || height <= 0)
 	return (NULL);
 
-array = (int**) malloc(height * sizeof(int*));
+array = (int **) malloc(height * sizeof(int *));
 if (array == NULL)
 	return (NULL);
 
 for (i = 0; i < height; i++)
 {
-	array[i] = (int*) malloc(width * sizeof(int));
+	array[i] = (int *) malloc(width * sizeof(int));
 	if (array[i] == NULL)
-		return (NULL);
+{
+	for (; i >= 0; i--)
+{
+		free(array[i]);
+}
+
+	free(array);
+	return (NULL);
+}
 }
 for (i = 0; i < height; i++)
 {
 	for (j = 0; j < width; j++)
-{
 		array[i][j] = 0;
-}
+
 }
 return (array);
 }
