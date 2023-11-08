9820c1f18106e28505118fb8796dad8faf74943e
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-11-07 20:30:21 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,13 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 
 /**
  *dog_t - typedef for struct dog
  */
 
-typedef struct dog dog_t;
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
