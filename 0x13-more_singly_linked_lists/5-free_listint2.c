33a12a2cb5edca6c422339c380cecff30eec0e7c
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-11-21 12:29:54 -0800

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - frees a list of list_t
+ *@head: pointer to the list
+ *Return: nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *current;
+
+	current = *head;
+	if (head != NULL)
+	{
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+		}
+	}
+	*head = NULL;
+}
