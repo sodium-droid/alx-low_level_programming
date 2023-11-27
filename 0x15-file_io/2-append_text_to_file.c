0e9b502134c35c173755854ce89a33cd05c6cdb6
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-11-27 12:19:06 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,26 +7,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    ssize_t bytes_written;
-    size_t content_len;
-    int fd;
+	ssize_t bytes_written;
+	size_t content_len;
+	int fd;
 
-if (filename == NULL)
-    return (-1);
-
-if (text_content == NULL)
-    return (1);
-
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 {
-    if (errno == ENOENT || errno == EACCES)
-        return (-1);
-    else 
-    {
-        perror("open");
-        return (-1);
-    }
+	if (errno == ENOENT || errno == EACCES)
+		return (-1);
+	{
+		perror("open");
+		return (-1);
+	}
 }
 content_len = strlen(text_content);
 bytes_written = write(fd, text_content, content_len);
@@ -34,8 +31,8 @@ close(fd);
 
 if (bytes_written == -1 || (size_t)bytes_written != content_len)
 {
-    perror("write");
-    return (-1);
+	perror("write");
+	return (-1);
 }
 return (1);
 }
