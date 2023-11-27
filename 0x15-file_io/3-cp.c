4e74a03288206008f2170d9e9c6b136d6bbfb346
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2023-11-27 10:12:54 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,71 @@
 #include "main.h"
-void error_exit(int code, const char *message, int fd1, int fd2);
 
 /**
- * main - Entry point
- * @argc: count of arguments
- * @argv: vectors
- * Return: 0 success
-*/
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - Entry point.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-	int fd_from, fd_to, close_file;
+	int file_from, file_to, err_close;
+	ssize_t read_chars, new;
 	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", "cp");
-		return (97);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	read_chars = BUFFER_SIZE;
+	while (read_chars == BUFFER_SIZE)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		return (98);
+		read_chars = read(file_from, buffer, 1024);
+		if (read_chars == -1)
+			error_file(-1, 0, argv);
+		new = write(file_to, buffer, read_chars);
+		if (new == -1)
+			error_file(0, -1, argv);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
-	if (fd_to == -1)
+
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
-		close(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		return (99);
-	}
-	while (bytes_read == BUFFER_SIZE)
-	{
-		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-			error_exit(98, "Error: Can't read from file", fd_from, fd_to);
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-			error_exit(99, "Error: Can't write to file", fd_from, fd_to);
-	}
-	close_file = close(fd_from);
-	if (close_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	close_file = close(fd_to);
-	if (close_file == -1)
+
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
-}
-/**
- * error_exit - print error message
- * @code: err code
- * @message: message to print
- * @fd1: file descriptor 1
- * @fd2: file descriptor 2
- * Return: nothing
-*/
-void error_exit(int code, const char *message, int fd1, int fd2)
-{
-	if (fd1 != -1)
-		close(fd1);
-	if (fd2 != -1)
-		close(fd2);
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
 }
