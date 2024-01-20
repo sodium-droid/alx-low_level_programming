583338655cfd07c2f4fbc44d9dc435f92b49d295
Ugochukwu Okpoko <ugochukwu712@gmail.com>
2024-01-20 04:30:37 -0800

    Advanced task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.github.com/sodium-droid/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hack.so
-export LD_PRELOAD=$PWD/../hack.so
+export LD_PRELOAD=../hack.so
